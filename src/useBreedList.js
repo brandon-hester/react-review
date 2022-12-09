f03cc9be20a28f2830d6d38100a19575732be122
Brandon Hester <brandonrhester@gmail.com>
2022-12-08 23:34:38 -0500

    custom hook

---
+++ src/useBreedList.js
@@ -0,0 +1,36 @@
+import { useState, useEFfect, useEffect } from "react";
+
+const localCache = {}; //should use localStorage for this
+
+export default function useBreedList(animal) {
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
+
+  useEffect(() => {
+    alert(status);
+  }, [status]);
+
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
+
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+      const json = await res.json();
+      localCache[animal] = json.breeds || [];
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
+
+  return [breedList, status];
+}
